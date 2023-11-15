f43ef9a70469b1d6c1a1d978a2b5a55b4ad77790
romainfrezier <romainfrezier@gmail.com>
2023-11-15 22:41:57 +0100

    feat(Custom pre authorize): Customized annotations for preauthorize.

--- src/main/java/fr/polytech/restcontroller/InvoiceController.java
+++ src/main/java/fr/polytech/restcontroller/InvoiceController.java
@@ -1,10 +1,16 @@
 package fr.polytech.restcontroller;
 
+import fr.polytech.annotation.IsAdmin;
+import fr.polytech.annotation.IsRecruiter;
+import fr.polytech.annotation.IsRecruiterOrAdmin;
 import fr.polytech.model.Invoice;
 import fr.polytech.model.InvoiceDataDTO;
 import fr.polytech.service.InvoiceService;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.Produces;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,55 +25,59 @@ public class InvoiceController {
     private InvoiceService invoiceService;
 
     @PostMapping("/")
+    @IsRecruiter
+    @Consumes(MediaType.APPLICATION_JSON_VALUE)
+    @Produces(MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Invoice> createInvoice(@RequestBody InvoiceDataDTO invoice) {
         try {
             return ResponseEntity.ok(invoiceService.createInvoice(invoice));
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
     }
 
     @GetMapping("/")
+    @IsAdmin
+    @Produces(MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Invoice>> getAllInvoices() {
         try {
             return ResponseEntity.ok(invoiceService.getAllInvoices());
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
     }
 
     @GetMapping("/{id}")
+    @IsRecruiterOrAdmin
+    @Produces(MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Invoice> getInvoiceById(@PathVariable("id") UUID id) {
         try {
             return ResponseEntity.ok(invoiceService.getInvoiceById(id));
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
     }
 
     @GetMapping("/url/{id}")
+    @IsRecruiterOrAdmin
+    @Produces(MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> getInvoiceUrlById(@PathVariable("id") UUID id) {
         try {
             return ResponseEntity.ok(invoiceService.getInvoiceUrl(id));
-        }
-        catch (NotFoundException e) {
+        } catch (NotFoundException e) {
             return ResponseEntity.notFound().build();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
     }
 
     @DeleteMapping("/{id}")
+    @IsRecruiterOrAdmin
     public ResponseEntity<Void> deleteInvoice(@PathVariable("id") UUID id) {
         try {
             invoiceService.deleteInvoice(id);
             return ResponseEntity.ok().build();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
     }
